4afc3243934962f680b43be25ab70437d3aadc49
sinyoungnam <sinnam90@gmail.com>
2018-09-03 01:01:29 -0400

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,19 @@
 class BoatingTest
 
+  attr_reader :name, :instructor
+  attr_accessor :status, :student
+
+  @@all = []
+
+  def initialize(name, status, instructor)
+    @name = name
+    @status = status
+    @instructor = instructor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,28 @@
 class Student
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, status, instructor)
+    new_boating_test = BoatingTest.new(test_name, status, instructor)
+    new_boating_test.student = self
+  end
+
+  def self.find_student(name)
+    @@all.find do |student|
+      student.name == name
+    end
+  end
+
 end
