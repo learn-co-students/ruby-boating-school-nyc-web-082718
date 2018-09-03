f98eef8f0df3abad61baa569e5a39f8b6e4cb146
github username <github email address>
2018-09-03 19:36:54 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,61 @@
+require "pry"
+
+# init with name
+# * return all instructors
+# * Instructor.pass_student should take in a student name and test name and return status failed
+# * Instructor.fail_student should take in a student name and test name and return status failed
+# * Instructor.student_grade_percentage should take in a student first name and output the percentage of tests that they have passed
+
+
+
 class Instructor
 
+attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.pass_student(student_name, test_name)
+    BoatingTest.all.each do |bt_obj|
+      if bt_obj.student.name == student_name && bt_obj.test_name == test_name
+        return "passed"
+      end
+    end
+  end
+
+  def self.fail_student(student_name, test_name)
+    BoatingTest.all.each do |bt_obj|
+      if bt_obj.student.name == student_name && bt_obj.test_name == test_name
+        return "failed"
+      end
+    end
+  end
+
+  def self.student_grade_percentage(student_name)
+    total_arr = []
+    passed_arr = []
+    BoatingTest.all.each do |bt_obj|
+      if bt_obj.student.name == student_name
+        total_arr << bt_obj.test_name
+      if bt_obj.test_status == "passed"
+        passed_arr << bt_obj.test_status
+      end
+    end
+  end
+    return (passed_arr.length)/(total_arr.length).to_f
+  end
+
 end
+
+
+# i1 = Instructor.new("Steve")
+# i2 = Instructor.new("Joe")
+# i3 = Instructor.new("Mary")
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,9 @@
+require "pry"
+
 require_relative '../config/environment.rb'
+# require_relative '../ruby-boating-school-nyc-web-082718/app/models/boatingtest.rb'
+# require_relative '../ruby-boating-school-nyc-web-082718/app/models/instructor.rb'
+# require_relative '../ruby-boating-school-nyc-web-082718/app/models/student.rb'
 
 def reload
   load 'config/environment.rb'
@@ -8,6 +13,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+i1 = Instructor.new("Steve")
+i2 = Instructor.new("Joe")
+i3 = Instructor.new("Mary")
+
+s1 = Student.new("Bobby")
+s2 = Student.new("Kiki")
+s3 = Student.new("Drake")
+
+bt1 = BoatingTest.new(s1, "floating", "passed", i2)
+bt2 = BoatingTest.new(s3, "turning", "failed", i2)
+bt3 = BoatingTest.new(s2, "speeding", "passed", i3)
+bt4 = BoatingTest.new(s2, "traffic", "failed", i1)
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
