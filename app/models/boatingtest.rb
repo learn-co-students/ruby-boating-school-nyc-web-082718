3b13427ccca8409604ffa4ffecb071eb22efcbb0
github username <github email address>
2018-09-03 12:50:54 -0400

    Solved all problems.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,18 @@
 class BoatingTest
+  attr_accessor :student, :instructor, :test_status, :test_name
 
+  @@all = []
+
+  def initialize(student, test_name, test_status, instructor)
+    @student = student
+    @instructor = instructor
+    @test_status = test_status
+    @test_name = test_name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
