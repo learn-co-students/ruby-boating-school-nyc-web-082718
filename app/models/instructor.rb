ea310aa8142872371f3effcfea8f0f2de6b464b7
JStern95 <JSternberg95@gmail.com>
2018-09-03 15:11:46 -0400

    Complete

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,45 @@
 class Instructor
 
+  attr_accessor :name, :test_name, :test_status, :instructor, :first_name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self,test_name, test_status, instructor)
+  end
+
+  def self.pass_student(first_name, test_name)
+    #binding.pry
+    passing = BoatingTest.all.find {|info| info.student.first_name == first_name && info.test_name == test_name}
+    passing.test_status = "passed"
+  end
+
+  def self.failed_student(first_name, test_name)
+    #binding.pry
+    failing = BoatingTest.all.find {|info| info.student.first_name == first_name && info.test_name == test_name}
+    failing.test_status = "failed"
+  end
+
+  def self.student_grade_percentage(first_name)
+    #select the student I'm looking to get info from
+    #save how many times he's taken the test
+    #find how many times he's got "passing" in BoatingTest
+    #Divide that into the number of all of his tests and multiply by 100
+    test_scores = BoatingTest.all.select {|info| info.student.first_name == first_name}
+    all_tests = test_scores.length.to_f
+    passing_tests = test_scores.select {|info| info.test_status == "passed"}
+    passing_total = passing_tests.length.to_f
+    final_answer = ((passing_total/all_tests)*100).to_i
+    "#{final_answer}%"
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,4 @@
+require "pry"
 require_relative '../config/environment.rb'
 
 def reload
@@ -8,6 +9,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+spongebob= Student.new("Spongebob")
+patrick= Student.new("Patrick")
+
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+
+test1= spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+test2= patrick.add_boating_test("Don't Crash 101", "pending", krabs)
+test6= spongebob.add_boating_test("Don't Crash 102", "passed", puff)
+test3= spongebob.add_boating_test("Don't Crash 103", "pending", puff)
+test5= spongebob.add_boating_test("Don't Crash 104", "pending", krabs)
+test4= spongebob.add_boating_test("Don't Crash 105", "pending", puff)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
