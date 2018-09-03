a8651c9ccb1c030b05086a8caf91018e9d4fddcf
Chris Bojemski <chris.bojemski@gmail.com>
2018-09-03 17:08:55 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,28 @@
 class Student
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(name)
+    @@all.find do |student|
+      student.name == name
+    end
+  end
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
 end
